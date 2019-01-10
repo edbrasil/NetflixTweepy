08f99a7d583138f45380611a8116ce1420c5171f
edbrasil <epbrasil717@gmail.com>
2019-01-10 09:53:59 -0500

    Project Setup

---
+++ flatten_tweets.py
@@ -0,0 +1,42 @@
+import json
+
+def flatten_tweets(tweets_json):
+    """ Flattens out tweet dictionaries so relevant JSON
+        is in a top-level dictionary."""
+    tweets_list = []
+    
+    # Iterate through each tweet
+    for tweet in tweets_json:
+        tweet_obj = json.loads(tweet)
+    
+        # Store the user screen name in 'user-screen_name'
+        tweet_obj['user-screen_name'] = tweet_obj['user']['screen_name']
+    
+        # Check if this is a 140+ character tweet
+        if 'extended_tweet' in tweet_obj:
+            # Store the extended tweet text in 'extended_tweet-full_text'
+            tweet_obj['extended_tweet-full_text'] = tweet_obj['extended_tweet']['full_text']
+    
+        if 'retweeted_status' in tweet_obj:
+            # Store the retweet user screen name in 'retweeted_status-user-screen_name'
+            tweet_obj['retweeted_status-user-screen_name'] = tweet_obj['retweeted_status']['user']['screen_name']
+
+            # Store the retweet text in 'retweeted_status-text'
+            tweet_obj['retweeted_status-text'] = tweet_obj['retweeted_status']['text']
+            
+        tweets_list.append(tweet_obj)
+    return tweets_list
+
+def check_word_in_tweet(word, data):
+    """Checks if a word is in a Twitter dataset's text. 
+    Checks text and extended tweet (140+ character tweets) for tweets,
+    retweets and quoted tweets.
+    Returns a logical pandas Series.
+    """
+    contains_column = data['text'].str.contains(word, case = False)
+    contains_column |= data['extended_tweet-full_text'].str.contains(word, case = False)
+    contains_column |= data['quoted_status-text'].str.contains(word, case = False)
+    contains_column |= data['quoted_status-extended_tweet-full_text'].str.contains(word, case = False)
+    contains_column |= data['retweeted_status-text'].str.contains(word, case = False)
+    contains_column |= data['retweeted_status-extended_tweet-full_text'].str.contains(word, case = False)
+    return contains_column
---
+++ setup.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan  7 13:38:31 2019
+
+@author: edbras
+"""
+
+import json
+from tweepy import OAuthHandler,API,Stream
+from slistener import SListener
+
+
+def load_cred():
+
+    with open('twitter_credentials.json') as cred_data:
+        info = json.load(cred_data)
+        consumer_key = info['CONSUMER_KEY']
+        consumer_secret = info['CONSUMER_SECRET']
+        access_key = info['ACCESS_KEY']
+        access_secret = info['ACCESS_SECRET']
+    
+    return consumer_key, consumer_secret, access_key, access_secret
+
+consumer_key, consumer_secret, access_token, access_token_secret = load_cred()
+
+#Authorization and initialization
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+# Set up the API with the authentication handler
+api = API(auth)
+
+# Set up words to track
+keywords_to_track = ('#Netflix','#LukeCage', '#IronFist', '#Daredevil')
+
+# Instantiate the SListener object 
+listen = SListener(api)
+
+# Instantiate the Stream object
+stream = Stream(auth, listen)
+
+# Begin collecting data
+stream.filter(track = keywords_to_track)
+print (stream)
+
+
+#Load JSON
+tweet_list = []
+with open('streamer_20190107-134421.json', 'r') as tweet_data:
+    tweets_json = filter(None,tweet_data.read().split("\n"))
+    
+    for tweet in tweets_json:
+        tweet_obj = json.loads(tweet)
+        tweet_list.append(tweet_obj)
+
+#print(tweet['text'])
+                     
+                     
+#Historical Tweets (API only allows 2 weeks)
+
+"""
+import csv
+from tweepy import Cursor
+csvFile = open('netflix.csv','w')
+
+csvWriter = csv.writer(csvFile)
+
+for tweet in Cursor(api.search, q = keywords_to_track, count=100,
+                           lang = 'en', since = '2018-11-01').items():
+    print(tweet.created_at, tweet.text)
+    csvWriter.writerow([tweet.created_at,tweet.text.encode('utf-8')])
+"""
